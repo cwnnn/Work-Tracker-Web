45b7007b846d47f36465f882af2e3796c2243db8
cwnnn <cangs026@gmail.com>
2025-11-01 01:14:28 +0300

    updated firebase in project

--- src/firebase.d.ts
+++
@@ -1,7 +0,0 @@
-// src/firebase.d.ts
-/*declare module './firebase' {
-  import { Auth, UserCredential } from 'firebase/auth'
-
-  export const auth: Auth
-  export function signInAnonymously(auth: Auth): Promise<UserCredential>
-}*/
