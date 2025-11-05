dcbf514d6189b86d0058ed2eab749d24fe1f6e77
cwnnn <cangs026@gmail.com>
2025-11-05 07:17:30 +0300

    add emty adminview

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import TestView from '../views/TestViev.vue'
 import DashboardView from '../views/DasboardView.vue'
+import AdminView from '@/views/AdminView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView,
+    },
+    {
+      path: '/admin',
+      name: 'amin',
+      component: AdminView,
     },
   ],
 })
---
+++ src/views/AdminView.vue
@@ -0,0 +1,4 @@
+<template><div></div></template>
+<script setup lang="ts">
+//local key deki isimlendirme hatasını düzelt ve işlem bitince sildir, birde boş bir zamanda resul.space de de vercel.json koy
+</script>
