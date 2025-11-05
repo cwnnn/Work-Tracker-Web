cfea6c4a0a4a4e99c7d3303240a0d8dade552ee4
cwnnn <cangs026@gmail.com>
2025-11-05 08:25:08 +0300

    emty

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,4 +1,2 @@
 <template><div></div></template>
-<script setup lang="ts">
-//local key deki isimlendirme hatasını düzelt ve işlem bitince sildir, birde boş bir zamanda resul.space de de vercel.json koy
-</script>
+<script setup lang="ts"></script>
