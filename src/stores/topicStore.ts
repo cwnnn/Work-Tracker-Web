b957009b5a7aa49fce8807e36cdefce3eea2e4c9
cwnnn <cangs026@gmail.com>
2025-11-13 23:51:50 +0300

    add updateTopicName for topicstore

--- src/stores/topicStore.ts
+++ src/stores/topicStore.ts
@@ -12,10 +12,21 @@ export const useTopicStore = defineStore('topic', () => {
   function addTopic(newTopic: UserTopic) {
     topics.value.push(newTopic)
   }
+
   function removeTopic(topicId: string) {
     topics.value = topics.value.filter((t) => t.id !== topicId)
     console.log(`🗑 store'dan topic silindi: ${topicId}`)
   }
 
-  return { topics, setTopics, addTopic, removeTopic }
+  function updateTopicName(topicId: string, newName: string) {
+    const topic = topics.value.find((t) => t.id === topicId)
+    if (topic) {
+      topic.topic = newName.trim()
+      console.log(`✏️ store'daki topic güncellendi: ${topicId} → ${newName}`)
+    } else {
+      console.warn(`⚠️ store'da böyle bir topic bulunamadı: ${topicId}`)
+    }
+  }
+
+  return { topics, setTopics, addTopic, removeTopic, updateTopicName }
 })
